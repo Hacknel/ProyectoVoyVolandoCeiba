733f0fb4825d7fb298d820f0757bb6491b1d8ae8
Samuel Oviedo <isaaccoviedo@gmail.com>
2020-06-27 19:08:58 -0600

    Diseño Login 2.0

--- ProyectoPEDVVoyVolandoCeiba/ProyectoPEDVVoyVolandoCeiba/Login/Login.Designer.vb
+++ ProyectoPEDVVoyVolandoCeiba/ProyectoPEDVVoyVolandoCeiba/Login/Login.Designer.vb
@@ -22,10 +22,181 @@ Partial Class Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.panelTitulo = New System.Windows.Forms.Panel()
+        Me.btnMinimizar = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnMostrarContra = New System.Windows.Forms.Button()
+        Me.panelTitulo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelTitulo
+        '
+        Me.panelTitulo.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(70, Byte), Integer))
+        Me.panelTitulo.Controls.Add(Me.btnMinimizar)
+        Me.panelTitulo.Controls.Add(Me.btnCerrar)
+        Me.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelTitulo.Location = New System.Drawing.Point(0, 0)
+        Me.panelTitulo.Name = "panelTitulo"
+        Me.panelTitulo.Size = New System.Drawing.Size(389, 55)
+        Me.panelTitulo.TabIndex = 0
+        '
+        'btnMinimizar
+        '
+        Me.btnMinimizar.BackgroundImage = CType(resources.GetObject("btnMinimizar.BackgroundImage"), System.Drawing.Image)
+        Me.btnMinimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMinimizar.FlatAppearance.BorderSize = 0
+        Me.btnMinimizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray
+        Me.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver
+        Me.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMinimizar.Location = New System.Drawing.Point(306, 12)
+        Me.btnMinimizar.Name = "btnMinimizar"
+        Me.btnMinimizar.Size = New System.Drawing.Size(35, 35)
+        Me.btnMinimizar.TabIndex = 1
+        Me.btnMinimizar.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.BackgroundImage = CType(resources.GetObject("btnCerrar.BackgroundImage"), System.Drawing.Image)
+        Me.btnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCerrar.FlatAppearance.BorderSize = 0
+        Me.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCerrar.Location = New System.Drawing.Point(346, 12)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(35, 35)
+        Me.btnCerrar.TabIndex = 0
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(70, Byte), Integer))
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 480)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(389, 15)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Fugaz One", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Silver
+        Me.Label1.Location = New System.Drawing.Point(26, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(202, 29)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Nombre de usuario:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(31, 139)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(310, 31)
+        Me.txtNombre.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Fugaz One", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Silver
+        Me.Label2.Location = New System.Drawing.Point(26, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(134, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Contraseña:"
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtContraseña.Location = New System.Drawing.Point(31, 237)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.Size = New System.Drawing.Size(310, 31)
+        Me.txtContraseña.TabIndex = 5
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnIngresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray
+        Me.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnIngresar.Font = New System.Drawing.Font("Staatliches", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIngresar.ForeColor = System.Drawing.Color.Silver
+        Me.btnIngresar.Location = New System.Drawing.Point(42, 331)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(310, 67)
+        Me.btnIngresar.TabIndex = 6
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Silver
+        Me.Panel2.Location = New System.Drawing.Point(61, 434)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(280, 2)
+        Me.Panel2.TabIndex = 7
+        '
+        'btnMostrarContra
+        '
+        Me.btnMostrarContra.BackgroundImage = Global.ProyectoPEDVVoyVolandoCeiba.My.Resources.Resources.icons8_show_password_64
+        Me.btnMostrarContra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnMostrarContra.FlatAppearance.BorderSize = 0
+        Me.btnMostrarContra.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray
+        Me.btnMostrarContra.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver
+        Me.btnMostrarContra.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMostrarContra.Location = New System.Drawing.Point(347, 231)
+        Me.btnMostrarContra.Name = "btnMostrarContra"
+        Me.btnMostrarContra.Size = New System.Drawing.Size(35, 35)
+        Me.btnMostrarContra.TabIndex = 8
+        Me.btnMostrarContra.UseVisualStyleBackColor = True
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(104, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(389, 495)
+        Me.Controls.Add(Me.btnMostrarContra)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.panelTitulo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Login"
+        Me.Opacity = 0.95R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.panelTitulo.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents panelTitulo As Panel
+    Friend WithEvents btnMinimizar As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtContraseña As TextBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnMostrarContra As Button
 End Class
--- ProyectoPEDVVoyVolandoCeiba/ProyectoPEDVVoyVolandoCeiba/Login/Login.vb
+++ ProyectoPEDVVoyVolandoCeiba/ProyectoPEDVVoyVolandoCeiba/Login/Login.vb
@@ -1,3 +1,52 @@
-﻿Public Class Login
+﻿Imports System.Runtime.InteropServices
+
+Public Class Login
+#Region "Form behaviors"
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        End
+
+    End Sub
+    Private Sub btnMinimizar_Click(sender As Object, e As EventArgs) Handles btnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+
+    End Sub
+#End Region
+
+#Region "Move form"
+    <DLLImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DLLImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, wMs As Integer, wParam As Integer, lParam As Integer)
+
+    End Sub
+
+    Private Sub panelTitulo_MouseMove(sender As Object, e As MouseEventArgs) Handles panelTitulo.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+
+    End Sub
+
+    Private Sub btnMostrarContra_Click(sender As Object, e As EventArgs) Handles btnMostrarContra.Click
+
+        If txtContraseña.UseSystemPasswordChar = True Then
+            txtContraseña.UseSystemPasswordChar = False
+
+        Else
+            txtContraseña.UseSystemPasswordChar = True
+
+        End If
+
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtContraseña.UseSystemPasswordChar = True
+
+    End Sub
+
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+
+    End Sub
+#End Region
 
 End Class
